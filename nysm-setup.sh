2f059f882adfabcbc8429d30d8fa669c3193c7c8
Adam Brown <audrummer15@gmail.com>
2018-01-24 17:29:48 -0500

    * Added SSL certificate automation.

--- nysm-setup.sh
+++ nysm-setup.sh
@@ -21,7 +21,7 @@ nysm_error() {
 }
 
 error_exit() {
-  echo "\n$1\n" 1>&2
+  echo -e "\n$1\n" 1>&2
   exit 1
 }
 
@@ -32,13 +32,19 @@ check_errors() {
   fi
 }
 
-create_socat() {
-  read -r -p "What port should socat listen on? " inbound_port
-  read -r -p "What IP/hostname should socat forward data to? " outbound_host
-  read -r -p "What port should socat forward data to? " outbound_port
+create_socat_instance() {
+  if [ -z "$1" ]; then
+    read -r -p "What port should socat listen on? " inbound_port
+  else
+    inbound_port=$1
+  fi
+
+  read -r -p "What IP/hostname should socat forward $2data to? " outbound_host
+  read -r -p "What port should socat forward $2data to? " outbound_port
 
   socat -d -d -lf /var/log/socat.log TCP-LISTEN:$inbound_port,fork TCP-CONNECT:$outbound_host:$outbound_port &
 
+  ps aux | grep socat | grep $inbound_port
   netstat -tlpn | grep socat | grep $inbound_port
 }
 
@@ -55,46 +61,65 @@ nysm_confirm() {
 }
 
 nysm_install() {
+  CONF_DST="/etc/nginx/sites-enabled/default"
+
   nysm_action "Installing Dependencies..."
+  apt-get install -y software-properties-common vim less
+
+  nysm_action "Adding certbot ppa..."
+  add-apt-repository -y ppa:certbot/certbot
+  check_errors
 
   nysm_action "Updating apt-get..."
   apt-get update
   check_errors
 
   nysm_action "Installing general net tools..."
-  apt-get install -y inetutils-ping net-tools screen dnsutils curl
+  apt-get install -y inetutils-ping net-tools screen dnsutils curl openssl openjdk-9-jre-headless
   check_errors
 
-  nysm_action "Installing nginx, socat..."
-  apt-get install -y socat nginx
+  nysm_action "Installing nginx, socat, certbot..."
+  apt-get install -y socat nginx python-certbot-nginx
   check_errors
 
   nysm_action "Finished installing dependencies!"
 }
 
-nysm_start() {
-  nysm_action "Starting servers..."
-
-  nysm_action "Starting nginx..."
-  service nginx start
+nysm_initialize() {
+  nysm_action "Modifying nginx configs..."
+  cp ./default.conf $CONF_DST
+  read -r -p "What is the sites domain name? (ex: google.com) " domain_name
+  sed -i.bak "s/server_name.*/server_name $domain_name;/" $CONF_DST
+  rm $CONF_DST.bak
   check_errors
-  nysm_action "Nginx is running!"
+
+  SSL_SRC="/etc/letsencrypt/live/$domain_name"
+  nysm_action "Installing Certificates..."
+  certbot --authenticator standalone --installer nginx --pre-hook "service nginx stop" --post-hook "service nginx start" -d $domain_name
+
+  nysm_action "Generating Keystore for Teamserver..."
+  read -r -p "Create an alphanumeric password for the keystore (double-check, no confirmation): " store_pass
+  openssl pkcs12 -export -in $SSL_SRC/fullchain.pem -inkey $SSL_SRC/privkey.pem -out $SSL_SRC/$domain_name.p12 -name $domain_name -passout pass:$store_pass
+  keytool -importkeystore -deststorepass $store_pass -destkeypass $store_pass -destkeystore $SSL_SRC/$domain_name.store -srckeystore $SSL_SRC/$domain_name.p12 -srcstoretype PKCS12 -srcstorepass $store_pass -alias $domain_name
 
   nysm_action "Starting socat..."
-  while nysm_confirm "Do you want to add an instance of socat?"; do
-    create_socat
-    printf "\n"
-    check_errors
-  done
+  create_socat_instance 15080 "HTTP "
+  create_socat_instance 15443 "HTTPS "
+
+  nysm_action "The keystore for your teamserver can be found at $SSL_SRC/$domain_name.store"
+}
+
+nysm_setup() {
+  nysm_install
+  nysm_initialize
 }
 
 nysm_status() {
   printf "\n************************ Processes ************************\n"
-  ps aux | grep nginx | grep -v grep
-  ps aux | grep socat | grep -v grep
+  ps aux | grep (socat|nginx) | grep -v grep
 
   printf "\n************************* Network *************************\n"
-  netstat -tulpn | grep socat
+  netstat -tulpn | grep -E '(socat|nginx)'
 }
 
 PS3="
@@ -103,20 +128,16 @@ NYSM - Select an Option:  "
 finshed=0
 while (( !finished )); do
   printf "\n"
-  options=("Install Dependencies" "Start Servers" "Add Socat Instance" "Check Status" "Quit")
+  options=("Setup NginX w/ Redirectors" "Add Socat Instance" "Check Status" "Quit")
   select opt in "${options[@]}"
   do
     case $opt in
-      "Install Dependencies")
-        nysm_install
-        break;
-        ;;
-      "Start Servers")
-        nysm_start
+      "Setup NginX w/ Redirectors")
+        nysm_setup
         break;
         ;;
       "Add Socat Instance")
-        create_socat
+        create_socat_instance
         break;
         ;;
       "Check Status")
